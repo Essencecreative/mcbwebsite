de901c95f21a804cdae5dab2efcdcdabadeef28b
kennethsimon <32138863+kennethsimon@users.noreply.github.com>
2026-02-02 11:36:16 +0300

    Use conditional rendering to hide map section completely

--- app/contact/page.js
+++ app/contact/page.js
@@ -265,8 +265,8 @@ export default function Home() {
                     {/*Contact Two End*/}
                    
                     {/*Google Map Start*/}
-                    {/* Entire Google Map Section Commented Out */}
-                    {/* 
+                    {/* Entire Google Map Section Commented Out - Using conditional rendering to ensure it's hidden */}
+                    {false && (
                     <section className="google-map">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
@@ -357,7 +357,7 @@ export default function Home() {
                             </div>
                         </div>
                     </section>
-                    */}
+                    )}
                     {/*Google Map End*/}
 
                     {/*CTA One Start*/}
